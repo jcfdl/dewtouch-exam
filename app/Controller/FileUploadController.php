257ed7c88cc400ea0d653813e8a43e2ee83c9e34
jcfdl <jc.deleon027@gmail.com>
2020-01-24 13:11:08 +0800

    question 4 done

--- app/Controller/FileUploadController.php
+++ app/Controller/FileUploadController.php
@@ -2,9 +2,52 @@
 
 class FileUploadController extends AppController {
 	public function index() {
+		$file_uploads = array();
+		$get_all = 0;
+		ini_set("auto_detect_line_endings", true);
 		$this->set('title', __('File Upload Answer'));
 
-		$file_uploads = $this->FileUpload->find('all');
+		if(isset($this->data['FileUpload'])) {
+			$last_rec = $this->FileUpload->find('first', array('order'=>array('id'=>'DESC')));
+			if(empty($last_rec)) {
+				$get_all = 1;
+			}
+			$csv_array = array();
+			$file = $this->data['FileUpload']['file'];
+			$name = explode(".", $file['name']);
+      $name = uniqid().".".$name[1];
+      $ext = substr(strtolower(strrchr($name, '.')), 1); 
+      $arr_ext = array('csv');
+      if(in_array($ext, $arr_ext)) {
+      	$csvfile = fopen($file['tmp_name'], "r");
+				$header = fgetcsv($csvfile);
+				while(($data = fgetcsv($csvfile, 1000, ",")) !== FALSE) {
+					$this->FileUpload->clear();
+					$this->FileUpload->save(array(
+						'name' => $data[0],
+						'email' => $data[1]
+					));
+				}
+				fclose($csvfile);
+				$message = 'Successfully imported CSV File';
+        $this->setFlash($message, array('class' => 'alert-success'));
+        if($get_all) {
+        	$this->redirect(array('controller' => 'FileUpload' , 'action' => 'index', 'all'=>1));
+        } else {
+        	$this->redirect(array('controller' => 'FileUpload' , 'action' => 'index', 'id'=>$last_rec['FileUpload']['id']));
+        }
+      } else {
+      	$message = 'Your file should only be a CSV file.';
+        $this->setFlash($message, array('class' => 'alert-danger'));
+        $this->redirect(array('controller' => 'FileUpload' , 'action' => 'index'));
+      }
+		}
+		if(isset($this->params['named']['all'])) {
+			$file_uploads = $this->FileUpload->find('all');
+		}
+		if(isset($this->params['named']['id'])) {
+			$file_uploads = $this->FileUpload->find('all', array('conditions'=>array('id >' => $this->params['named']['id'])));
+		}
 		$this->set(compact('file_uploads'));
 	}
 }
